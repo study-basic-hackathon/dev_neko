503cd3aa59d0bdc58af1d15fb97eecdaddc4bcff
Yamazaki Takeshi <yamazaki@uhc.jp>
2025-06-21 14:10:17 +0900

    DBの接続設定を環境変数を使うようにした

--- code/src/lib/db.ts
+++ code/src/lib/db.ts
@@ -1,9 +1,9 @@
 import mysql from 'mysql2/promise';
 
 export const db = mysql.createPool({
-  host: 'db',
-  user: 'noder',
-  password: 'secret',
-  database: 'dev_neko',
-  port: 3306,
+  host: process.env.MYSQL_HOST || 'localhost',
+  user: process.env.MYSQL_USER || 'noder',
+  password: process.env.MYSQL_PASSWORD || 'secret',
+  database: process.env.MYSQL_DB || 'dev_neko',
+  port: process.env.MYSQL_PORT ? parseInt(process.env.MYSQL_PORT, 10) : 3306,
 });
